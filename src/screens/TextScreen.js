d7366f67e3d0a336ef1f605929c11918d004ab56
bdafahim <badrudduza20@student.sust.edu>
2020-01-20 15:33:25 +0600

    primary style in textinput

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -4,9 +4,17 @@ import { Text, StyleSheet, View, Button, TextInput } from "react-native";
 const TextScreen = () => {
   return (
     <View>
-      <TextInput />
+      <TextInput style={styles.input} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1
+  }
+});
 
 export default TextScreen;
