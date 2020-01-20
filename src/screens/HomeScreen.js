f38349607433aa6e8bb170d51e08921e3e063985
bdafahim <badrudduza20@student.sust.edu>
2020-01-20 16:17:24 +0600

    Touchable opacity  instead of button homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,36 +4,72 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 const HomeScreen = ({ navigation }) => {
   return (
     <View>
-      <Text style={styles.text}>Welcome</Text>
+      <Text style={styles.textHome}>Welcome</Text>
 
-      <Button
+      {/* <Button
+        style={styles.buttonStyle}
         onPress={() => navigation.navigate("Library")}
         title="Go to Library"
-      />
-      <Button
+      /> */}
+      {/* <Button
         onPress={() => navigation.navigate("DateTime")}
         title="Go to Date time Picker"
-      />
-      <Button
-        onPress={() => navigation.navigate("Count")}
-        title="Go to Counter Screen"
-      />
-      <Button
+      /> */}
+      {/* <Button
         onPress={() => navigation.navigate("Color")}
         title="Add a color"
-      />
-      <Button
+      /> */}
+      {/* <Button
         onPress={() => navigation.navigate("Square")}
         title="Square Screen"
-      />
-      <Button onPress={() => navigation.navigate("Text")} title="Text Screen" />
+      /> */}
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Library")}
+        style={styles.buttonStyle}
+      >
+        <Text style={styles.text}>Go to library</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Color")}
+        style={styles.buttonStyle}
+      >
+        <Text style={styles.text}>Add a color</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Square")}
+        style={styles.buttonStyle}
+      >
+        <Text style={styles.text}>Square Screen</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Text")}
+        style={styles.buttonStyle}
+      >
+        <Text style={styles.text}>Text Screen</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
+    fontSize: 20,
+    color: "white"
+  },
+  textHome: {
+    marginBottom: 40,
+    fontSize: 30,
+    color: "#f92b2b"
+  },
+  buttonStyle: {
+    margin: 10,
+    alignItems: "center",
+    backgroundColor: "#ff7070",
+    padding: 3
   }
 });
 
