5d9e6c5004a05c55b6c0fe02b4484dc0c182f482
bdafahim <badrudduza20@student.sust.edu>
2020-02-04 08:34:03 +0600

    Card added

--- App.js
+++ App.js
@@ -6,29 +6,31 @@ import CounterSceen from "./src/screens/CounterSceen";
 import ColorScreen from "./src/screens/ColorScreen";
 import SquareScreen from "./src/screens/SquareScreen";
 import TextScreen from "./src/screens/TextScreen";
+import Card from "./src/screens/Card";
 import React from "react";
 import { createAppContainer } from "react-navigation";
 const navigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Library: BasicFlatList,
-    DateTime: DateTimePickerTester,
-    Count: CounterSceen,
-    Color: ColorScreen,
-    Square: SquareScreen,
-    Text: TextScreen
-  },
-  {
-    initialRouteName: "Home",
-    defaultNavigationOptions: {
-      title: "App"
+    {
+        Home: HomeScreen,
+        Library: BasicFlatList,
+        DateTime: DateTimePickerTester,
+        Count: CounterSceen,
+        Color: ColorScreen,
+        Square: SquareScreen,
+        Text: TextScreen,
+        card: Card
+    },
+    {
+        initialRouteName: "Home",
+        defaultNavigationOptions: {
+            title: "App"
+        }
     }
-  }
 );
 
 const AppContainer = createAppContainer(navigator);
 export default class App extends React.Component {
-  render() {
-    return <AppContainer />;
-  }
+    render() {
+        return <AppContainer />;
+    }
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,67 +2,74 @@ import React, { Component } from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 export default class HomeScreen extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <View>
-        <Text style={styles.textHome}>Welcome</Text>
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <View>
+                <Text style={styles.textHome}>Welcome</Text>
 
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate("DateTime")}
-          style={styles.buttonStyle}
-        >
-          <Text style={styles.text}>Date Time picker</Text>
-        </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate("DateTime")}
+                    style={styles.buttonStyle}
+                >
+                    <Text style={styles.text}>Date Time picker</Text>
+                </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate("Library")}
-          style={styles.buttonStyle}
-        >
-          <Text style={styles.text}>Go to library</Text>
-        </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate("Library")}
+                    style={styles.buttonStyle}
+                >
+                    <Text style={styles.text}>Go to library</Text>
+                </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate("Color")}
-          style={styles.buttonStyle}
-        >
-          <Text style={styles.text}>Add a color</Text>
-        </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate("Color")}
+                    style={styles.buttonStyle}
+                >
+                    <Text style={styles.text}>Add a color</Text>
+                </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate("Square")}
-          style={styles.buttonStyle}
-        >
-          <Text style={styles.text}>Square Screen</Text>
-        </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate("Square")}
+                    style={styles.buttonStyle}
+                >
+                    <Text style={styles.text}>Square Screen</Text>
+                </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate("Text")}
-          style={styles.buttonStyle}
-        >
-          <Text style={styles.text}>Text Screen</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate("Text")}
+                    style={styles.buttonStyle}
+                >
+                    <Text style={styles.text}>Text Screen</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate("card")}
+                    style={styles.buttonStyle}
+                >
+                    <Text style={styles.text}>Card view</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 20,
-    color: "white"
-  },
-  textHome: {
-    marginBottom: 40,
-    fontSize: 30,
-    color: "#f92b2b"
-  },
-  buttonStyle: {
-    margin: 10,
-    alignItems: "center",
-    backgroundColor: "#ff7070",
-    padding: 3
-  }
+    text: {
+        fontSize: 20,
+        color: "white"
+    },
+    textHome: {
+        marginBottom: 40,
+        fontSize: 30,
+        color: "#f92b2b"
+    },
+    buttonStyle: {
+        margin: 10,
+        alignItems: "center",
+        backgroundColor: "#ff7070",
+        padding: 3
+    }
 });
