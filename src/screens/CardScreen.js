b7ee99826531c1f60f833cd7c27c089b88867045
bdafahim <badrudduza20@student.sust.edu>
2020-02-04 17:12:52 +0600

    Latest News

--- src/screens/CardScreen.js
+++ src/screens/CardScreen.js
@@ -1,61 +1,92 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 export default class CardScreen extends Component {
     state = {};
     render() {
         return (
-            <View style={styles.container}>
-                <View
-                    style={{
-                        flex: 1,
-                        flexDirection: "column"
-                    }}
-                >
+            <View
+                style={{
+                    flex: 1,
+                    flexDirection: "column"
+                }}
+            >
+                <View style={styles.container}>
                     <View
                         style={{
                             flex: 1,
-                            flexDirection: "row",
-                            marginTop: 10
+                            flexDirection: "column"
                         }}
                     >
-                        <Image
-                            source={require("../../assets/forest.jpg")}
-                            style={styles.cardImage}
-                        ></Image>
                         <View
                             style={{
                                 flex: 1,
-                                flexDirection: "column",
-                                marginLeft: 10
+                                flexDirection: "row",
+                                marginTop: 10
                             }}
                         >
-                            <Text style={styles.HeaderText}>
-                                Mark's daily digest
-                            </Text>
-                            <Text style={styles.DescriptionText}>
-                                For November 27, 2019
-                            </Text>
+                            <Image
+                                source={require("../../assets/forest.jpg")}
+                                style={styles.cardImage}
+                            ></Image>
+                            <View
+                                style={{
+                                    flex: 1,
+                                    flexDirection: "column",
+                                    marginLeft: 10
+                                }}
+                            >
+                                <Text style={styles.HeaderText}>
+                                    Mark's daily digest
+                                </Text>
+                                <Text style={styles.DescriptionText}>
+                                    For November 27, 2019
+                                </Text>
+                            </View>
+                        </View>
+                        <View
+                            style={{
+                                flex: 1,
+                                flexDirection: "row",
+                                marginTop: 30,
+                                marginBottom: 25
+                            }}
+                        >
+                            <View
+                                style={{
+                                    flex: 1,
+                                    flexDirection: "column",
+                                    alignItems: "center"
+                                }}
+                            >
+                                <Text style={styles.NewsCounterText}>14</Text>
+                                <Text style={styles.DescriptionText}>
+                                    Tax News
+                                </Text>
+                            </View>
                         </View>
                     </View>
+                </View>
+
+                <View
+                    style={{
+                        marginLeft: 10,
+                        marginRight: 10,
+                        marginTop: 30,
+                        flex: 1,
+                        flexDirection: "row"
+                    }}
+                >
+                    <Text style={styles.taxNewsTitle}>Latest Tax News</Text>
                     <View
                         style={{
                             flex: 1,
-                            flexDirection: "row",
-                            marginTop: 30,
-                            marginBottom: 25
+                            alignItems: "flex-end"
                         }}
                     >
-                        <View
-                            style={{
-                                flex: 1,
-                                flexDirection: "column",
-                                alignItems: "center"
-                            }}
-                        >
-                            <Text style={styles.NewsCounterText}>14</Text>
-                            <Text style={styles.DescriptionText}>Tax News</Text>
-                        </View>
+                        <TouchableOpacity onPress={() => "#"}>
+                            <Text style={styles.ViewAllText}>View all</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
             </View>
@@ -92,5 +123,17 @@ const styles = StyleSheet.create({
         fontSize: 35,
         color: "#ffffff",
         fontWeight: "bold"
+    },
+
+    taxNewsTitle: {
+        fontSize: 20,
+        padding: 5,
+        color: "#000000",
+        fontWeight: "bold"
+    },
+    ViewAllText: {
+        fontSize: 20,
+        padding: 5,
+        color: "#6a04b4"
     }
 });
